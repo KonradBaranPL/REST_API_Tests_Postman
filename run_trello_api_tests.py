424bef7d24f7e42af97a42f57ef3c18a227d3e8e
KonradBaranPL <112825533+KonradBaranPL@users.noreply.github.com>
2025-09-29 10:15:25 +0200

    Update run_trello_api_tests.py
    change "command" variable syntax

--- run_trello_api_tests.py
+++ run_trello_api_tests.py
@@ -1,7 +1,13 @@
 import subprocess
 
 # Definicja komendy do uruchomienia
-command = "newman.cmd run trello_requests_collection.json -e trello_environment.json --reporters cli,html --reporter-html-export reports\trello_api_test_report.html"
+command = (
+    "newman.cmd run trello_requests_collection.json"
+    "-e trello_environment.json"
+    "--reporters cli,html"
+    "--reporter-html-export"
+    "reports\\trello_api_test_report.html"
+)
 
 # Uruchomienie komendy
 result = subprocess.run(command.split(), capture_output=True, encoding="utf-8")
